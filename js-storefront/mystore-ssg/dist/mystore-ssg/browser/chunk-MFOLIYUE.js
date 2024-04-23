2f4ecd44d120ff3d92e4c6abb7448d6e579622f1
cristianojulianocosta <cristiano.costa@sap.com>
2024-04-22 21:11:46 -0300

    dist

---
+++ js-storefront/mystore-ssg/dist/mystore-ssg/browser/chunk-MFOLIYUE.js
@@ -0,0 +1,1 @@
+import{a,b}from"./chunk-FRXET4RT.js";import"./chunk-7L2XTRYB.js";import"./chunk-6ZBOZGRC.js";import"./chunk-74LPZTBL.js";export{a as AddToCartComponent,b as AddToCartModule};
---
+++ js-storefront/mystore-ssg/dist/mystore-ssg/server/chunk-64PFO4CM.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a,b}from"./chunk-DMABUUK4.mjs";import"./chunk-VVGRI3Y4.mjs";import"./chunk-7QBZJS7B.mjs";import"./chunk-NYYLCXVW.mjs";import"./chunk-GBQV73GG.mjs";import"./chunk-MCGIIYOL.mjs";export{a as AddToCartComponent,b as AddToCartModule};
